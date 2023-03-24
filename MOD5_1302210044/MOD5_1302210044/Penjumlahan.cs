ad01967694ee6fe4cea9066373a56b5891f512d4
shieldadchr <shielda.diasavitri@gmail.com>
2023-03-24 08:55:30 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ MOD5_1302210044/MOD5_1302210044/Penjumlahan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD5_1302210044
+{
+    public class Penjumlahan
+    {
+        private dynamic data;
+        public Penjumlahan() { }
+        public void PenjumlahanTigaAngka<T>(T a, T b, T c)
+        {
+            dynamic aa= a;  
+            dynamic bb= b;
+            dynamic cc= c;
+
+            data = aa+ bb + cc;   
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Hasil Penjumlahan: {this.data}");
+        }
+    }
+
+}
--- MOD5_1302210044/MOD5_1302210044/Program.cs
+++ MOD5_1302210044/MOD5_1302210044/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MOD5_1302210044;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Penjumlahan penjumlahan = new Penjumlahan();
+        penjumlahan.PenjumlahanTigaAngka<double>(13, 02, 44);
+        penjumlahan.Print();
+    }
+}
+    
